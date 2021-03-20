11a969e66f6906bfb10fbcf5eefae5c5396afae3
Yash Singh <saiansh2525@gmail.com>
2021-03-19 18:44:20 -0700

    Init

---
+++ README.md
@@ -0,0 +1,29 @@
+# `node-dedupredup`
+
+Solution for removing duplicates and rebuilding the directory in `NodeJS`. Useful for directories and repositories with duplicate files or folders.
+
+## API
+
+### Example
+
+```js
+const { dedup, redup } = require('node-dedupredup');
+dedup('directory/with/duplicates');
+redup('previously/deduped/directory/to/rebuild');
+```
+
+When running `dedup`, a `.redup.json` file will be created at the root of the directory specified as the argument. This JSON file will contain all of the information for rebuilding the directory. The `redup` method will not work without a `.redup.json` file.
+
+### dedup
+
+- Type: `function`
+- Paramaters: `dir`: `string`
+
+The dedup function will take the `dir` given and remove duplicate files. The result that will be returned will be the contents of the new `.redup.json` (parsed).
+
+### redup
+
+- Type: `function`
+- Paramaters: `dir`: `string`
+
+The redup function will rebuild the `dir` directory given using the `.redup.json` present.
---
+++ index.js
@@ -0,0 +1,93 @@
+const fs = require('fs');
+const path = require('path');
+
+function recursiveGetFiles(dir) {
+  let files = [];
+  fs.readdirSync(dir)
+    .map((entry) => path.join(dir, entry))
+    .forEach((entryPath) => {
+      let pathInfo = fs.statSync(entryPath);
+      if (pathInfo.isDirectory()) {
+        files = files.concat(recursiveGetFiles(entryPath));
+      } else {
+        files.push({ name: entryPath, length: pathInfo.size });
+      }
+    });
+  return files;
+}
+
+function getEmptyDirectories(dir) {
+  let emptyDirs = [];
+  fs.readdirSync(dir)
+    .map((entry) => path.join(dir, entry))
+    .filter((pathName) => fs.statSync(pathName).isDirectory())
+    .forEach((directory) => {
+      if (fs.readdirSync(directory).length === 0) {
+        emptyDirs.push(directory);
+      } else {
+        emptyDirs.concat(getEmptyDirectories(directory));
+      }
+    });
+  return emptyDirs;
+}
+
+module.exports = {
+  /**
+   * Remove duplicate files from a directory
+   * @param {string} dir The directory to remove duplicate files from
+   * @returns {{ emptyDirectories: Array<String>, matchingFiles: Object<Array<String>> }} The resulting .redup.json (it is already written)
+   */
+  dedup(dir = '.') {
+    let files = recursiveGetFiles(dir);
+    let lengthObj = Object.fromEntries(files.map((file) => [file.length, []]));
+    files.forEach((file) => {
+      lengthObj[file.length].push(file.name);
+    });
+    lengthObj = Object.fromEntries(Object.entries(lengthObj).filter((entry) => entry.length > 1));
+    lengthObj = Object.values(lengthObj).flatMap((lengthGroup) => {
+      let contentGroup = [];
+      lengthGroup.forEach((fileName) => {
+        let fileContent = fs.readFileSync(fileName, 'utf8');
+        let contentFind = contentGroup.find((group) => group.content === fileContent);
+        if (contentFind) {
+          contentFind.name.push(fileName);
+        } else {
+          contentGroup.push({ name: [fileName], content: fileContent });
+        }
+      });
+      return contentGroup;
+    });
+    let emptyDirectories = new Set(getEmptyDirectories(dir));
+    lengthObj.forEach((group) => {
+      group.name.slice(1).forEach((name) => fs.unlinkSync(name));
+    });
+    emptyDirectories = new Set([...new Set(getEmptyDirectories(dir))].filter((x) => !emptyDirectories.has(x)));
+    emptyDirectories.forEach((emptyDir) => {
+      fs.rmdirSync(emptyDir);
+    });
+    let redupJSON = {
+      emptyDirectories: [...emptyDirectories].map((dirName) => path.relative('.', dirName)),
+      matchingFiles: Object.fromEntries(lengthObj.map((group) => [group.name[0], group.name.slice(1)]).filter((group) => group[1].length > 0))
+    };
+    if (Object.keys(redupJSON.matchingFiles).length === 0) return;
+    fs.writeFileSync(path.join(dir, '.redup.json'), JSON.stringify(redupJSON));
+    return redupJSON;
+  },
+  /**
+   * Rebuild the directory after deduping it
+   * @param {string} dir The directory to rebuild
+   */
+  redup(dir = '.') {
+    let config = JSON.parse(fs.readFileSync(path.join(dir, '.redup.json'), 'utf8'));
+    config.emptyDirectories.forEach((emptyDir) => {
+      fs.mkdirSync(emptyDir);
+    });
+    Object.entries(config.matchingFiles).forEach((file) => {
+      let fileContent = fs.readFileSync(file[0], 'utf8');
+      file[1].forEach((removedFile) => {
+        fs.writeFileSync(removedFile, fileContent);
+      });
+    });
+    fs.unlinkSync(path.join(dir, '.redup.json'));
+  }
+};
